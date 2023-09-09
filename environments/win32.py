d894d7176c6366c5b8d32cc171ee47d6ef79a247
Kat Inskip <kat@inskip.me>
2023-09-08 21:05:45 -0700

    win32: registry changes for span / fit

--- environments/win32.py
+++ environments/win32.py
@@ -1,6 +1,7 @@
 import os
 import ctypes
 import logging
+import winreg
 from imager import combine_to_viewport
 from module_loader import add_environment
 
@@ -21,8 +22,14 @@ This sets wallpapers on Windows.
 def set_wallpapers(files: list, displays: list):
     if len(files) > 1:
         logging.debug("Several monitors detected, going the hard route")
+        desktop = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, winreg.KEY_ALL_ACCESS)
+        wallpaper_style = winreg.SetValueEx(desktop, "WallpaperStyle", 0, winreg.REG_SZ, "5")
+        desktop.Close()
         file = combine_to_viewport(displays, files)
         ctypes.windll.user32.SystemParametersInfoW(20, 0, file, 0)
     else:
         logging.debug("Detected only one monitor, setting wagilpaper simply")
+        desktop = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, winreg.KEY_ALL_ACCESS)
+        wallpaper_style = winreg.SetValueEx(desktop, "WallpaperStyle", 0, winreg.REG_SZ, "3")
+        desktop.Close()
         ctypes.windll.user32.SystemParametersInfoW(20, 0, files[0] , 0)
