a42a01299bede0d631155d1c4a2fc919a84146bd
Kat Inskip <kat@inskip.me>
2025-02-28 00:39:16 -0800

    fix: missing ] on show_url

--- konawall/gui.py
+++ konawall/gui.py
@@ -92,7 +92,7 @@ class Konawall(wx.adv.TaskBarIcon):
 
     def open_url(self, evt=None):
         for post in self.current:
-            subprocess.call(["xdg-open", post["show_url"])
+            subprocess.call(["xdg-open", post["show_url"]])
 
     def open_log(self, evt=None):
         subprocess.call(["xdg-open", self.log_path])
